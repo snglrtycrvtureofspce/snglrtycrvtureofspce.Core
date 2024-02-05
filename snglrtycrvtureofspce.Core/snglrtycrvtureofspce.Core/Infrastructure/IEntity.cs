1c47654c0cdfd3a339b649a730229dbeb8c8d0cb
snglrtycrvtureofspce <asp1rantzenevich@gmail.com>
2024-02-05 23:08:19 +0300

    Initial package

---
+++ snglrtycrvtureofspce.Core/snglrtycrvtureofspce.Core/Infrastructure/IEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace snglrtycrvtureofspce.Core.Infrastructure;
+
+/// <summary>
+/// Base entity interface
+/// </summary>
+public interface IEntity
+{
+    #region IEntity
+    Guid Id { get; set; }
+    
+    DateTime CreatedDate { get; set; }
+    
+    DateTime ModificationDate { get; set; }
+    #endregion
+}
---
+++ snglrtycrvtureofspce.Core/snglrtycrvtureofspce.Core/Infrastructure/ValidationErrorResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace snglrtycrvtureofspce.Core.Infrastructure;
+
+/// <summary>
+/// Represents a validation error response returned in case of a 400 Bad Request status with validation errors.
+/// </summary>
+public class ValidationErrorResponse
+{
+    public string Type { get; set; }
+    
+    public string Title { get; set; }
+    
+    public int Status { get; set; }
+    
+    public string TraceId { get; set; }
+    
+    public Dictionary<string, string[]> Errors { get; set; }
+}
