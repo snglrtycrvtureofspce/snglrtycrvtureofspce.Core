e9956d2b690c88a0a2f7f0f773d719971364f4e1
snglrtycrvtureofspce <asp1rantzenevich@gmail.com>
2024-04-11 05:13:11 +0300

    added ComplexTypes and JwtAuth

---
+++ snglrtycrvtureofspce.Core/Base/ComplexTypes/SystemUsersEnumeration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace snglrtycrvtureofspce.Core.Base.ComplexTypes;
+
+public class SystemUsersEnumeration : TypeEnumeration
+{
+    public static readonly SystemUsersEnumeration SystemMessage =
+        new SystemUsersEnumeration(nameof(SystemMessage), Guid.Parse("00000000-0000-0000-0001-000000000000"));
+
+    public Guid Identificator { get; set; }
+
+    public SystemUsersEnumeration(string value, Guid id) : base(value)
+    {
+        Identificator = id;
+    }
+
+    public override bool Equals(object obj)
+    {
+        return Identificator.Equals(((SystemUsersEnumeration)obj).Identificator);
+    }
+}
---
+++ snglrtycrvtureofspce.Core/Base/ComplexTypes/TypeEnumeration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace snglrtycrvtureofspce.Core.Base.ComplexTypes;
+
+public abstract class TypeEnumeration : IComparable
+{
+    public string Value { get; set; }
+
+    public TypeEnumeration(string value) => Value = value;
+
+    public override string ToString() => Value;
+
+    public virtual int CompareTo(object obj)
+    {
+        return string.Compare(Value, ((TypeEnumeration) obj).Value, StringComparison.Ordinal);
+    }
+
+    public static IEnumerable<T> GetAll<T>() where T : TypeEnumeration
+    {
+        return ((IEnumerable<FieldInfo>) typeof (T)
+            .GetFields(BindingFlags.DeclaredOnly | BindingFlags.Static | BindingFlags.Public))
+            .Select<FieldInfo, object>((Func<FieldInfo, object>) (f => f.GetValue((object) null)))
+            .Cast<T>();
+    }
+}
