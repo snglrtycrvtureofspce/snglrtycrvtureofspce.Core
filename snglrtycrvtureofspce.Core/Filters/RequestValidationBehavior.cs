88f15fa7c649d1812c0472fb11956bc7b07e148e
snglrtycrvtureofspce <asp1rantzenevich@gmail.com>
2024-09-23 20:30:21 +0300

    1.0.24

--- snglrtycrvtureofspce.Core/Filters/RequestValidationBehavior.cs
+++ snglrtycrvtureofspce.Core/Filters/RequestValidationBehavior.cs
@@ -1,35 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using FluentValidation;
-using FluentValidation.Results;
 using MediatR;
 
 namespace snglrtycrvtureofspce.Core.Filters;
 
-public class RequestValidationBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators) :
-    IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+public class RequestValidationBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators) 
+    : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
 {
-    public Task<TResponse> Handle(
-        TRequest request,
-        RequestHandlerDelegate<TResponse> next,
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, 
         CancellationToken cancellationToken)
     {
-        var context = new ValidationContext<TRequest>(request);
-        
-        var list = validators
-            .Select((Func<IValidator<TRequest>, ValidationResult>) (v => v.Validate(context)))
-            .SelectMany((Func<ValidationResult, IEnumerable<ValidationFailure>>) (result => result.Errors))
-            .Where((Func<ValidationFailure, bool>) (_ => true))
-            .ToList();
-        
-        if (list.Count != 0)
+        if (validators.Any())
         {
-            throw new ValidationException(list);
+            var context = new ValidationContext<TRequest>(request);
+            
+            var validationResults = await Task.WhenAll(
+                validators.Select(v => v.ValidateAsync(context, cancellationToken))
+            );
+
+            var failures = validationResults
+                .SelectMany(result => result.Errors)
+                .Where(error => error != null)
+                .ToList();
+
+            if (failures.Count != 0)
+            {
+                throw new ValidationException(failures);
+            }
         }
         
-        return next();
+        return await next();
     }
 }
