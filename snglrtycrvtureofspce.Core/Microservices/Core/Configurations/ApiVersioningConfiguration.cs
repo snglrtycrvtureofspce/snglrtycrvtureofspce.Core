ef050254cf46aba44d6c1d22d524ab5062670483
snglrtycrvtureofspce <asp1rantzenevich@gmail.com>
2024-10-01 22:12:50 +0300

    1.0.27

---
+++ snglrtycrvtureofspce.Core/Microservices/Core/Configurations/ApiVersioningConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Asp.Versioning;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace snglrtycrvtureofspce.Core.Microservices.Core.Configurations;
+
+public static class ApiVersioningConfiguration
+{
+    public static void ConfigureApiVersioning(this IServiceCollection services)
+    {
+        services.AddApiVersioning(options =>
+        {
+            options.ReportApiVersions = true;
+            options.DefaultApiVersion = new ApiVersion(1.0);
+            options.AssumeDefaultVersionWhenUnspecified = true;
+            options.ApiVersionReader = ApiVersionReader.Combine(new HeaderApiVersionReader("apiVersion"));
+                
+            options.Policies.Sunset(0.9)
+                .Effective(DateTimeOffset.Now.AddDays(60))
+                .Link("policy.html")
+                .Title("Versioning Policy")
+                .Type("text/html");
+        }).AddMvc().AddApiExplorer(options =>
+        {
+            options.GroupNameFormat = "'v'VVV";
+            options.SubstituteApiVersionInUrl = true;
+        });
+    }
+}
