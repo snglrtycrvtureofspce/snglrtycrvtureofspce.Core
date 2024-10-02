912043ffadc8afd6ce1e1bfc54c901d910329b03
snglrtycrvtureofspce <asp1rantzenevich@gmail.com>
2024-10-03 01:25:38 +0300

    1.0.28

---
+++ snglrtycrvtureofspce.Core/Base/Infrastructure/IAuditable.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace snglrtycrvtureofspce.Core.Base.Infrastructure;
+
+public interface IAuditable : IEntity
+{
+    public Guid CreatedBy { get; set; }
+    
+    public Guid ModifiedBy { get; set; }
+}
