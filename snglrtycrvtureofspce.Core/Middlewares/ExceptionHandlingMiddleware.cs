ae790b4d9c6a13d120126470418440d177213548
snglrtycrvtureofspce <asp1rantzenevich@gmail.com>
2024-10-03 04:47:29 +0300

    1.0.29

---
+++ snglrtycrvtureofspce.Core/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentValidation;
+using Microsoft.AspNetCore.Http;
+
+namespace snglrtycrvtureofspce.Core.Middlewares;
+
+public class ExceptionHandlingMiddleware(RequestDelegate next)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (ValidationException ex)
+        {
+            await HandleValidationExceptionAsync(context, ex);
+        }
+        catch (Exception ex)
+        {
+            await HandleGlobalExceptionAsync(context, ex);
+        }
+    }
+
+    private static Task HandleValidationExceptionAsync(HttpContext context, ValidationException ex)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = StatusCodes.Status400BadRequest;
+
+        var response = new
+        {
+            message = "Validation error",
+            details = ex.Errors
+        };
+
+        return context.Response.WriteAsJsonAsync(response);
+    }
+
+    private static Task HandleGlobalExceptionAsync(HttpContext context, Exception ex)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+        var response = new
+        {
+            message = "An unexpected error occurred",
+            details = ex.Message
+        };
+
+        return context.Response.WriteAsJsonAsync(response);
+    }
+}
