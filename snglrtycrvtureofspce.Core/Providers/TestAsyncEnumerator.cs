29840b2b2be4ff520dca306faa4ead445e039392
snglrtycrvtureofspce <asp1rantzenevich@gmail.com>
2025-01-28 04:34:52 +0300

    1.0.35

---
+++ snglrtycrvtureofspce.Core/Providers/TestAsyncEnumerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace snglrtycrvtureofspce.Core.Providers;
+
+public class TestAsyncEnumerator<T>(IEnumerator<T> inner) : IAsyncEnumerator<T>
+{
+    public T Current => inner.Current;
+
+    public ValueTask DisposeAsync()
+    {
+        inner.Dispose();
+        return ValueTask.CompletedTask;
+    }
+
+    public ValueTask<bool> MoveNextAsync()
+    {
+        return new ValueTask<bool>(inner.MoveNext());
+    }
+}
---
+++ snglrtycrvtureofspce.Core/Providers/TestAsyncQueryProvider.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace snglrtycrvtureofspce.Core.Providers;
+
+// Auxiliary class for asynchronous provider
+public class TestAsyncQueryProvider<T>(IQueryProvider inner) : IAsyncQueryProvider
+{
+    public IQueryable CreateQuery(Expression expression)
+    {
+        return new TestAsyncEnumerable<T>(expression);
+    }
+
+    public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
+    {
+        return new TestAsyncEnumerable<TElement>(expression);
+    }
+
+    public object? Execute(Expression expression)
+    {
+        return inner.Execute(expression);
+    }
+
+    public TResult Execute<TResult>(Expression expression)
+    {
+        return inner.Execute<TResult>(expression);
+    }
+
+    public TResult ExecuteAsync<TResult>(Expression expression, CancellationToken cancellationToken = default)
+    {
+        var expectedResultType = typeof(TResult).GetGenericArguments()[0];
+        var executionResult = typeof(IQueryProvider)
+            .GetMethod(nameof(IQueryProvider.Execute), 1, new[] { typeof(Expression) })
+            ?.MakeGenericMethod(expectedResultType)
+            .Invoke(this, new[] { expression });
+
+        return (TResult)typeof(Task).GetMethod(nameof(Task.FromResult))
+            .MakeGenericMethod(expectedResultType)
+            .Invoke(null, new[] { executionResult });
+    }
+}
